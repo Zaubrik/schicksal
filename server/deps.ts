a65ffe9749246a5f33a37905766de1461b01636c
timonson <timo.rossa@tutanota.com>
2023-12-23 15:42:35 +0100

    Bump

--- server/deps.ts
+++ server/deps.ts
@@ -7,4 +7,4 @@ export {
   type CryptoKeyOrUpdateInput,
   getJwtFromBearer,
   verifyJwt,
-} from "https://dev.zaubrik.com/portal@v0.2.5/functions/mod.ts";
+} from "https://dev.zaubrik.com/portal@v0.2.6/functions/mod.ts";
